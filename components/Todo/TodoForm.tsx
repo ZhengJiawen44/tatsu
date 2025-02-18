860eaa69ff9e1f746ee8d5d9cd22ca08d91cccbd
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-02-18 19:19:27 +0800

    todo form logic refactor

--- components/Todo/TodoForm.tsx
+++ components/Todo/TodoForm.tsx
@@ -1,42 +1,37 @@
 import clsx from "clsx";
 import React, { useEffect, useRef, useState } from "react";
+import adjustHeight from "@/lib/adjustTextareaHeight";
+
 interface TodoFormProps {
   displayForm: boolean;
   setDisplayForm: React.Dispatch<boolean>;
 }
+
 const TodoForm = ({ displayForm, setDisplayForm }: TodoFormProps) => {
   const titleRef = useRef<null | HTMLInputElement>(null);
+  const textareaRef = useRef<null | HTMLTextAreaElement>(null);
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
 
   useEffect(() => {
+    adjustHeight(textareaRef);
     if (displayForm) {
       titleRef.current?.focus();
     }
   }, [displayForm]);
-  const textareaRef = useRef<null | HTMLTextAreaElement>(null);
-  const adjustHeight = () => {
-    const textarea = textareaRef.current;
-    if (textarea) {
-      // Reset height to min to accurately calculate new height
-      textarea.style.height = "auto";
-      // Set new height based on scrollHeight
-      textarea.style.height = `${textarea.scrollHeight}px`;
-    }
-  };
-
-  useEffect(() => {
-    // Initial height adjustment
-    adjustHeight();
-  }, []);
 
   return (
     <div>
       <form
         className={clsx(
-          "flex flex-col my-4 border rounded-md p-3 ",
+          "flex flex-col my-4 border rounded-md p-3 gap-3",
           !displayForm && "hidden"
         )}
       >
         <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           ref={titleRef}
           className="bg-transparent placeholder-card-foreground-muted text-card-foreground text-[1.2rem] font-semibold focus:outline-none"
           type="text"
@@ -45,19 +40,28 @@ const TodoForm = ({ displayForm, setDisplayForm }: TodoFormProps) => {
         />
 
         <textarea
+          value={desc}
           ref={textareaRef}
-          onChange={adjustHeight}
-          className="h-auto bg-transparent placeholder-card-foreground-muted text-card-foreground focus:outline-none resize-none"
+          onChange={(e) => {
+            setDesc(e.target.value);
+            adjustHeight(textareaRef);
+          }}
+          className="overflow-hidden bg-transparent placeholder-card-foreground-muted text-card-foreground font-extralight focus:outline-none resize-none"
           name="description"
-          defaultValue="description"
-        ></textarea>
+          placeholder="description"
+        />
 
-        <div className="mt-3 w-full h-1 border-b-[1px]"></div>
+        <div className="mt-1 w-full h-1 border-b-[1px]"></div>
         {/* buttons tooltip */}
         <div className="flex gap-5 items-center mt-3 mr-0 ml-auto">
           <button
+            type="button"
             className="bg-card-muted px-1 leading-none py-[2.5px] h-fit rounded-sm font-semibold"
-            onClick={() => setDisplayForm(!displayForm)}
+            onClick={() => {
+              setDesc("");
+              setTitle("");
+              setDisplayForm(!displayForm);
+            }}
           >
             cancel
           </button>
---
+++ lib/adjustTextareaHeight.ts
@@ -0,0 +1,18 @@
+import React from "react";
+
+const adjustTextareaHeight = (
+  textareaRef: React.RefObject<null | HTMLTextAreaElement>
+) => {
+  const textarea = textareaRef.current;
+  if (textarea) {
+    console.log(textarea.scrollHeight);
+
+    // Reset height to min to accurately calculate new height
+    textarea.style.height = "1rem";
+    // Set new height based on scrollHeight
+    if (textarea.scrollHeight !== 0)
+      textarea.style.height = `${textarea.scrollHeight}px`;
+  }
+};
+
+export default adjustTextareaHeight;
