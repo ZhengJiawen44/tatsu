1a89cea1cfe7c39bc527f0b276009423a8123a5e
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-03-20 01:59:49 +0800

    (chore) add thankyou message for first time user tryouts

--- features/todos/component/TodoContainer.tsx
+++ features/todos/component/TodoContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CreateTodoBtn from "./CreateTodoBtn";
 import PreviousTodo from "./PreviousTodo";
 import { useTodo } from "../api/get-todo";
@@ -9,6 +9,9 @@ import TodayTodos from "./TodayTodos";
 import PinnedTodos from "./PinnedTodos";
 import { useNotificaton } from "@/providers/NotificationProvider";
 import { useToast } from "@/hooks/use-toast";
+import Plus from "@/components/ui/icon/plus";
+import clsx from "clsx";
+import LineSeparator from "@/components/ui/lineSeparator";
 
 const TodoContainer = () => {
   //get all todos
@@ -43,10 +46,48 @@ const TodoContainer = () => {
     }
   );
 
+  const [showNotification, setShowNotification] = useState(() => {
+    return JSON.parse(localStorage.getItem("showNotification") || "true");
+  });
+
+  useEffect(() => {
+    localStorage.setItem("showNotification", JSON.stringify(showNotification));
+  }, [showNotification]);
+
   if (todoLoading) <TodoListLoading />;
 
   return (
     <div className="select-none bg-card">
+      <div
+        className={clsx(
+          "relative w-full my-4 rounded-lg border border-gray-700 bg-gray-800 p-4 text-[0.9rem] text-gray-300 shadow-md",
+          !showNotification && "hidden"
+        )}
+      >
+        <div className="pr-8">
+          <p className="mb-1">
+            Thank you for trying out
+            <span className="font-semibold text-white">Tatsu.gg</span>! We
+            appreciate you exploring our demo and value your feedback.
+          </p>
+          <p>
+            A special shoutout to
+            <span className="text-yellow-400 font-semibold">
+              JustPowerful Plays
+            </span>
+            for catching an issue where the sidebar toggle shortcut conflicts
+            with the rich text editor’s bold command.
+            <span className="italic"> You have my gratitude.</span>
+          </p>
+        </div>
+        <button
+          className="absolute top-2 right-2 text-gray-400 hover:text-white transition"
+          onClick={() => setShowNotification(false)}
+        >
+          <Plus className="w-5 h-5 rotate-45" />
+        </button>
+      </div>
+
       <Day />
       {/* Render Unpinned previous todos */}
       <PreviousTodo
