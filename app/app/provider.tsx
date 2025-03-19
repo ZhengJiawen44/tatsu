d34ad1c68aef8f4264e2759436889dd0a312e4bd
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-03-20 02:05:49 +0800

    (chore) moved system notification to root client layout

--- app/app/provider.tsx
+++ app/app/provider.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SidebarContainer from "@/components/Sidebar/SidebarContainer";
 import UserCard from "@/components/Sidebar/User/UserCard";
 import VaultItem from "@/components/Sidebar/Vault/VaultItem";
@@ -13,9 +13,17 @@ import clsx from "clsx";
 import SidebarIcon from "@/components/ui/icon/sidebar";
 import SidebarToggle from "@/components/ui/SidebarToggle";
 import { Toaster } from "@/components/ui/toaster";
+import Plus from "@/components/ui/icon/plus";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   const { showMenu, isResizing } = useMenu();
+  const [showNotification, setShowNotification] = useState(() => {
+    return JSON.parse(localStorage.getItem("showNotification") || "true");
+  });
+
+  useEffect(() => {
+    localStorage.setItem("showNotification", JSON.stringify(showNotification));
+  }, [showNotification]);
   return (
     <PassKeyProvider>
       <NotificationProvider>
@@ -41,6 +49,36 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
                   <SidebarIcon className="w-6 h-6" />
                 </SidebarToggle>
               )}
+              <div
+                className={clsx(
+                  "relative w-full my-4 rounded-lg border border-gray-700 bg-gray-800 p-4 text-[0.9rem] text-gray-300 shadow-md",
+                  !showNotification && "hidden"
+                )}
+              >
+                <div className="pr-8">
+                  <p className="mb-1">
+                    Thank you for trying out
+                    <span className="font-semibold text-white">Tatsu.gg</span>!
+                    We appreciate you exploring our demo and value your
+                    feedback.
+                  </p>
+                  <p>
+                    A special shoutout to
+                    <span className="text-yellow-400 font-semibold">
+                      JustPowerful Plays
+                    </span>
+                    for catching an issue where the sidebar toggle shortcut
+                    conflicts with the rich text editor’s bold command.
+                    <span className="italic"> You have my gratitude.</span>
+                  </p>
+                </div>
+                <button
+                  className="absolute top-2 right-2 text-gray-400 hover:text-white transition"
+                  onClick={() => setShowNotification(false)}
+                >
+                  <Plus className="w-5 h-5 rotate-45" />
+                </button>
+              </div>
               {children}
             </div>
           </div>
