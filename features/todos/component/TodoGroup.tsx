b04f28f2ad6b1f7299f480d4fb3e8d8aec4cd645
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-05-17 00:02:18 +0800

    (bug fix):redundat touch sensor causing touch to fail on todo item

--- features/todos/component/TodoGroup.tsx
+++ features/todos/component/TodoGroup.tsx
@@ -4,7 +4,6 @@ import { useSensor } from "@dnd-kit/core";
 import { useSensors } from "@dnd-kit/core";
 import { PointerSensor } from "@dnd-kit/core";
 import { KeyboardSensor } from "@dnd-kit/core";
-import { TouchSensor } from "@dnd-kit/core";
 import { closestCenter } from "@dnd-kit/core";
 import { arrayMove } from "@dnd-kit/sortable";
 import { verticalListSortingStrategy } from "@dnd-kit/sortable";
@@ -58,7 +57,6 @@ const TodoGroup = ({
 
   const sensors = useSensors(
     useSensor(PointerSensor),
-    useSensor(TouchSensor),
     useSensor(KeyboardSensor)
   );
 
