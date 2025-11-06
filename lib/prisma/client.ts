b70a21c99675fad6db889c3d59484a4644394bc5
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-11-06 16:39:34 +0800

    (feat) added database logging to identify page with high db usage

--- lib/prisma/client.ts
+++ lib/prisma/client.ts
@@ -4,6 +4,14 @@ const globalPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalPrisma.prisma ?? new PrismaClient();
+// Enable query logging in development
+export const prisma =
+  globalPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV !== "production"
+        ? ["query", "info", "warn", "error"]
+        : [],
+  });
 
 if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prisma;
