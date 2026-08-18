608d9e31e0f30ff1254fbd519188b41f234d09ff
Zheng Jiawen <zhengjiawen44@gmail.com>
2026-08-18 20:04:04 +0800

    refactor(sync): light refactor on todo instance sync

--- app/api/todo/instance/[id]/route.ts
+++ app/api/todo/instance/[id]/route.ts
@@ -69,11 +69,11 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
       const component = parseIcsToVeventComponent(oldIcs)
       const allVevents = component.getAllSubcomponents('vevent')
 
-      let isInstanceFound = false;
+      let instanceExists = false;
       const instance = allVevents.find((vevent)=>{
         const recurenceID = vevent.getFirstProperty("recurrence-id")?.getFirstValue() as ICAL.Time;
         if(recurenceID && recurenceID.toJSDate().toString() === instanceDate.toString()){
-          isInstanceFound = true
+          instanceExists = true
           return true
         }
         return false ;
@@ -84,6 +84,14 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
       if(!master) throw new InternalError("event master not found")
       const masterDtstart = master.getFirstPropertyValue("dtstart") as unknown as ICAL.Time
       
+      if(!instanceExists){
+        populateMetaProperties(instance, master)
+        instance.addPropertyWithValue("recurrence-id", toMasterShapedTime(instanceDate, masterDtstart))
+        if (tzid) 
+          instance.getFirstProperty('recurrence-id')!.setParameter('tzid', tzid)
+        component.addSubcomponent(instance)
+      }
+
       if(title) instance.updatePropertyWithValue("summary", title)
       if(description) instance.updatePropertyWithValue("description", description)
       if(dtstart) instance.updatePropertyWithValue("dtstart", toMasterShapedTime(dtstart, masterDtstart))
@@ -94,14 +102,6 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
         instance.getFirstProperty('dtend')!.setParameter('tzid', tzid)
       }
 
-      if(!isInstanceFound){
-        populateMetaProperties(instance, master)
-        instance.addPropertyWithValue("recurrence-id", toMasterShapedTime(instanceDate, masterDtstart))
-         if (tzid) 
-        instance.getFirstProperty('recurrence-id')!.setParameter('tzid', tzid)
-        component.addSubcomponent(instance)
-      }
-      
       const updatedIcs = component.toString();
       const { calDavClient } = await createCaldavClientFromDB(user.id)
       const res = await calDavClient.updateCalendarObject({
@@ -111,7 +111,7 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
           data: updatedIcs
         }
       })
-       const etag = res.headers.get('etag') ?? ''
+      const etag = res.headers.get('etag') ?? ''
       if (!etag) throw new InternalError('error updating remote calendar Objects')
       //sync local sync data
       await prisma.syncMetaData.update({
@@ -119,6 +119,7 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
         data: { etag, icsData: updatedIcs }
       })
     }
+    
     await prisma.todoInstance.upsert({
       where: {
         todoId_instanceDate: {
